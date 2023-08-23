d1a0b589aeb7c04daef26b503011de25924ae5d1
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-08-23 15:45:49 +0200

    install tabler icons and add basic skills card

---
+++ src/Skills.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Title = styled.h3`
+  text-align: center;
+`;
+
+const DIVVY = styled.div`
+height: 15rem;
+width: 20rem;
+background: magenta;
+margin: 1rem;
+`;
+
+export default function Skills() {
+    return(
+        <DIVVY>
+            <Title>hello</Title>
+        </DIVVY>
+    );
+}
