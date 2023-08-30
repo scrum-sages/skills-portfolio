cfddd507b583fe976fcec3c181ec722df6fcc6c3
mims <113519976+MimPMC@users.noreply.github.com>
2023-08-30 12:17:49 +0200

    Update data.ts

--- src/components/data.ts
+++ src/components/data.ts
@@ -1,5 +1,5 @@
 export interface HeaderLinks extends Array<string> {}
 
-export const headerLinks: HeaderLinks = ['TEAM', 'SKILLS', 'PROJECTS', 'CONTACT'];
+export const headerLinks: HeaderLinks = ['TEAM', 'PROJECTS', 'SKILLS', 'CONTACT'];
 
 export const headerTextColor: string = "black"
