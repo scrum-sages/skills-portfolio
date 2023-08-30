7223df4cb349c27a150334de49297bcef9a934a9
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-08-30 10:54:57 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 import "./App.css";
 import Map from "./components/Map";
+import Skills from "./Skills";
 
 function App() {
   return (
     <>
+    <Skills />
       <MapWrapper>
         <Map />
       </MapWrapper>
     </>
   );
 }
+
+const MapWrapper = styled.div`
+  height: 20rem;
+  width: 20rem;
+`;
 
 export default App;
