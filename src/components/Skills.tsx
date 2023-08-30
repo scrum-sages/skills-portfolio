8396554dbb906c4dfbef4c7b30bc2437a923c9ea
mims <113519976+MimPMC@users.noreply.github.com>
2023-08-30 16:58:17 +0200

    max width stroke width

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,17 +1,37 @@
+import {
+  IconBrandAdobe,
+  IconBrandCss3,
+  IconBrandCypress,
+  IconBrandFigma,
+  IconBrandGithub,
+  IconBrandHtml5,
+  IconBrandJavascript,
+  IconBrandMantine,
+  IconBrandMongodb,
+  IconBrandSocketIo,
+  IconBrandTypescript,
+  IconBrandVscode,
+  IconChevronDown,
+  IconChevronUp,
+} from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { IconBrandGithub, IconBrandVscode, IconBrandCss3, IconBrandHtml5, IconBrandJavascript, IconBrandSocketIo, IconChevronUp, IconChevronDown, IconBrandTypescript, IconBrandFigma, IconBrandAdobe, IconBrandCypress, IconBrandMantine, IconBrandMongodb } from '@tabler/icons-react';
-import { useState, useEffect } from 'react';
 
 export interface Data {
-    title: string;
-    icon: string;
-    details: string;
+  title: string;
+  icon: string;
+  details: string;
 }
 
 const Background = styled.div`
-background: #ECA579;
-max-width: 1440px;
-width: 100%;
+  background: #eca579;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 1440px;
 `;
 
 const Flex = styled.div`
@@ -29,9 +49,9 @@ const Title = styled.h4`
 `;
 
 const Buttons = styled.div`
-display: flex;
-justify-content: center;
-font-weight: 600;
+  display: flex;
+  justify-content: center;
+  font-weight: 600;
 `;
 
 const TransparentButton = styled.button`
@@ -46,150 +66,200 @@ const TransparentButton = styled.button`
   gap: 0.5rem;
 `;
 
-
 const IconWrapper = styled.div`
   display: inline-block;
   margin-top: 2rem;
+  color: black;
 `;
 
 const Card = styled.div`
-height: 10rem;
-width: 10rem;
-margin: 4.5rem;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  height: 10rem;
+  width: 10rem;
+  margin: 4.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-@media (max-width: 768px) {
+  @media (max-width: 768px) {
     margin: 1rem;
   }
 `;
 
-
 export const data: Data[] = [
-    {
-        title: "HTML",
-        icon: "IconBrandHtml5",
-        details: "HTML.",
-    },
-    {
-        title: "CSS",
-        icon: "IconBrandCss3",
-        details: "CSS.",
-    },
-    {
-        title: "JavaScript",
-        icon: "IconBrandJavascript",
-        details: "JavaScript.",
-    },
-    {
-        title: "TypeScript",
-        icon: "IconBrandTypescript",
-        details: "TypeScript has been used in several bigger projects, such as e-commerce sites.",
-    },
-    {
-        title: "GitHub",
-        icon: "IconBrandGithub",
-        details: "We have used GitHub to work on group projects.",
-    },
-    {
-        title: "Figma",
-        icon: "IconBrandFigma",
-        details: "We have used Figma to design wireframes and make prototypes as well as visually communicate ideas.",
-    },
-    {
-        title: "Cypress",
-        icon: "IconBrandCypress",
-        details: "Cypress has been used for testing our front end web applications.",
-    },
-    {
-        title: "Socket.io",
-        icon: "IconBrandSocketIo",
-        details: "Used to build a chat application for the web.",
-    },
-    {
-        title: "MongoDB",
-        icon: "IconBrandMongodb",
-        details: "Database hosting for e-commerce site, chat app and twitter copy. Hosts user info like username, encrypted password, posts or products as well as additional metadata associated with these.",
-    },
-    {
-        title: "Mantine UI",
-        icon: "IconBrandMantine",
-        details: "Design system used in e-commerce and chat app projects.",
-    },
-    {
-        title: "VS Code",
-        icon: "IconBrandVscode",
-        details: "Vs Code ",
-    },
-    {
-        title: "Adobe Suite",
-        icon: "IconBrandAdobe",
-        details: "Illustrator, Photoshop and InDesign were used to create graphic profiles and wireframes as well as mock ups of logos on products.",
-    },
+  {
+    title: 'HTML',
+    icon: 'IconBrandHtml5',
+    details: 'HTML.',
+  },
+  {
+    title: 'CSS',
+    icon: 'IconBrandCss3',
+    details: 'CSS.',
+  },
+  {
+    title: 'JavaScript',
+    icon: 'IconBrandJavascript',
+    details: 'JavaScript.',
+  },
+  {
+    title: 'TypeScript',
+    icon: 'IconBrandTypescript',
+    details: 'TypeScript has been used in several bigger projects, such as e-commerce sites.',
+  },
+  {
+    title: 'GitHub',
+    icon: 'IconBrandGithub',
+    details: 'We have used GitHub to work on group projects.',
+  },
+  {
+    title: 'Figma',
+    icon: 'IconBrandFigma',
+    details:
+      'We have used Figma to design wireframes and make prototypes as well as visually communicate ideas.',
+  },
+  {
+    title: 'Cypress',
+    icon: 'IconBrandCypress',
+    details: 'Cypress has been used for testing our front end web applications.',
+  },
+  {
+    title: 'Socket.io',
+    icon: 'IconBrandSocketIo',
+    details: 'Used to build a chat application for the web.',
+  },
+  {
+    title: 'MongoDB',
+    icon: 'IconBrandMongodb',
+    details:
+      'Database hosting for e-commerce site, chat app and twitter copy. Hosts user info like username, encrypted password, posts or products as well as additional metadata associated with these.',
+  },
+  {
+    title: 'Mantine UI',
+    icon: 'IconBrandMantine',
+    details: 'Design system used in e-commerce and chat app projects.',
+  },
+  {
+    title: 'VS Code',
+    icon: 'IconBrandVscode',
+    details: 'Vs Code ',
+  },
+  {
+    title: 'Adobe Suite',
+    icon: 'IconBrandAdobe',
+    details:
+      'Illustrator, Photoshop and InDesign were used to create graphic profiles and wireframes as well as mock ups of logos on products.',
+  },
+];
 
-]
-
-const icons: { [key: string]: JSX.Element} = {
-    IconBrandGithub: <IconWrapper><IconBrandGithub size={80} /></IconWrapper>,
-    IconBrandFigma: <IconWrapper><IconBrandFigma size={80} /></IconWrapper>,
-    IconBrandCypress: <IconWrapper><IconBrandCypress size={80} /></IconWrapper>,
-    IconBrandTypescript: <IconWrapper><IconBrandTypescript size={80} /></IconWrapper>,
-    IconBrandSocketIo: <IconWrapper><IconBrandSocketIo size={80} /></IconWrapper>,
-    IconBrandMantine: <IconWrapper><IconBrandMantine size={80} /></IconWrapper>,
-    IconBrandMongodb: <IconWrapper><IconBrandMongodb size={80} /></IconWrapper>,
-    IconBrandAdobe: <IconWrapper><IconBrandAdobe size={80} /></IconWrapper>,
-    IconBrandJavascript: <IconWrapper><IconBrandJavascript size={80} /></IconWrapper>,
-    IconBrandHtml5: <IconWrapper><IconBrandHtml5 size={80} /></IconWrapper>,
-    IconBrandCss3: <IconWrapper><IconBrandCss3 size={80} /></IconWrapper>,
-    IconBrandVscode: <IconWrapper><IconBrandVscode size={80} /></IconWrapper>
+const icons: { [key: string]: JSX.Element } = {
+  IconBrandGithub: (
+    <IconWrapper>
+      <IconBrandGithub size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandFigma: (
+    <IconWrapper>
+      <IconBrandFigma size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandCypress: (
+    <IconWrapper>
+      <IconBrandCypress size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandTypescript: (
+    <IconWrapper>
+      <IconBrandTypescript size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandSocketIo: (
+    <IconWrapper>
+      <IconBrandSocketIo size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandMantine: (
+    <IconWrapper>
+      <IconBrandMantine size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandMongodb: (
+    <IconWrapper>
+      <IconBrandMongodb size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandAdobe: (
+    <IconWrapper>
+      <IconBrandAdobe size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandJavascript: (
+    <IconWrapper>
+      <IconBrandJavascript size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandHtml5: (
+    <IconWrapper>
+      <IconBrandHtml5 size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandCss3: (
+    <IconWrapper>
+      <IconBrandCss3 size={80} stroke={1} />
+    </IconWrapper>
+  ),
+  IconBrandVscode: (
+    <IconWrapper>
+      <IconBrandVscode size={80} stroke={1} />
+    </IconWrapper>
+  ),
 };
 
-
-
-
-
 export default function Skills() {
-    const [displayData, setDisplayData] = useState<Data[]>([]);
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [displayData, setDisplayData] = useState<Data[]>([]);
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-    // Define how many cards per row and how many rows you want to initially display
-    const cardsPerRow = 4; 
-    const initialRows = 2; 
+  // Define how many cards per row and how many rows you want to initially display
+  const cardsPerRow = 4;
+  const initialRows = 2;
 
-    const handleDisplayLess = () => {
-        setIsExpanded(false);
-        setDisplayData(data.slice(0, cardsPerRow * initialRows));
-    };
+  const handleDisplayLess = () => {
+    setIsExpanded(false);
+    setDisplayData(data.slice(0, cardsPerRow * initialRows));
+  };
 
-    const handleExpand = () => {
-        setIsExpanded(true);
-        setDisplayData(data);
-    };
+  const handleExpand = () => {
+    setIsExpanded(true);
+    setDisplayData(data);
+  };
 
-    useEffect(() => {
-        setDisplayData(data.slice(0, cardsPerRow * initialRows));
-    }, []);
+  useEffect(() => {
+    setDisplayData(data.slice(0, cardsPerRow * initialRows));
+  }, []);
 
-
-
-    return(
-        <Background>
-            <Flex>
-            {displayData.map((item, index) => (
-                <Card key={index}>
-                    {icons[item.icon]}
-                    <Title>{item.title}</Title>
-                </Card>
-            ))}
-            </Flex>
-            <Buttons>
-            {isExpanded ? 
-            <TransparentButton onClick={handleDisplayLess}>Close <IconChevronUp /></TransparentButton> : 
-            <TransparentButton onClick={handleExpand}>See the complete list <IconChevronDown /></TransparentButton>
-            }
-            </Buttons>
-        </Background>
-    );
+  return (
+    <Background>
+      <ContentWrapper>
+        <Flex>
+          {displayData.map((item, index) => (
+            <Card key={index}>
+              {icons[item.icon]}
+              <Title>{item.title}</Title>
+            </Card>
+          ))}
+        </Flex>
+        <Buttons>
+          {isExpanded ? (
+            <TransparentButton onClick={handleDisplayLess}>
+              Close <IconChevronUp />
+            </TransparentButton>
+          ) : (
+            <TransparentButton onClick={handleExpand}>
+              See the complete list <IconChevronDown />
+            </TransparentButton>
+          )}
+        </Buttons>
+      </ContentWrapper>
+    </Background>
+  );
 }
