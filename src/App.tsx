ba5893e4b7649ef48bf057cd6ec9666a654fa0e1
Jennifer Techel <jennifertechel@hotmail.com>
2023-08-30 14:15:38 +0200

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import styled from "styled-components";
-import "./App.css";
-import Map from "./components/Map";
+import styled from 'styled-components';
+import './App.css';
+import Map from './components/Map';
+import Team from './components/Team';
 
 function App() {
   return (
     <>
+      <Team />
       <MapWrapper>
         <Map />
       </MapWrapper>
