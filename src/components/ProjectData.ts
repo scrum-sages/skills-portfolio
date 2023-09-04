e7bc417b5234a375e92843b19eeb73c869e346f0
Jennifer Techel <jennifertechel@hotmail.com>
2023-09-04 09:58:08 +0200

    create data file for projects

---
+++ src/components/ProjectData.ts
@@ -0,0 +1,32 @@
+export interface ProjectData {
+  title: string;
+  imageURL: string;
+  details: string;
+}
+
+export const ProjectList: ProjectData[] = [
+  {
+    title: 'OUR CASE',
+    imageURL: 'by-groove.svg',
+    details:
+      'Something about the skill and how awesome we are and some.  Something about the skill and how awesome we are and some. Something about the skill and how awesome we are and some. Something about the skill and how awesome.',
+  },
+  {
+    title: 'ANOTHER CASE',
+    imageURL: 'by-groove.svg',
+    details:
+      'Something about the skill and how awesome we are and some.  Something about the skill and how awesome we are and some. Something about the skill and how awesome we are and some. Something about the skill and how awesome.',
+  },
+  {
+    title: 'PREVIOUS CASE',
+    imageURL: 'by-groove.svg',
+    details:
+      'Something about the skill and how awesome we are and some.  Something about the skill and how awesome we are and some. Something about the skill and how awesome we are and some. Something about the skill and how awesome.',
+  },
+  {
+    title: 'CASE CASE CASE',
+    imageURL: 'by-groove.svg',
+    details:
+      'Something about the skill and how awesome we are and some.  Something about the skill and how awesome we are and some. Something about the skill and how awesome we are and some. Something about the skill and how awesome.',
+  },
+];
