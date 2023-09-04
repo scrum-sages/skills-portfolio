156c2a4b1a645b45e77802ee7ba10f65a046dfe9
Jennifer Techel <jennifertechel@hotmail.com>
2023-09-04 21:49:05 +0200

    add link to button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,25 @@
 import { styled } from 'styled-components';
 import { ProjectList } from './ProjectData'; // Import your data file
 
-function Projects() {
+interface Props {
+  navRef: React.RefObject<HTMLDivElement>;
+}
+
+function Projects({ navRef }: Props) {
   return (
-    <Container>
+    <Container ref={navRef}>
       <Heading>PROJECTS</Heading>
       <ProjectSection>
         {ProjectList.map((project, index) => (
           <ProjectItem key={index}>
             <Image src={project.imageURL} alt={project.title} />
-            <h3>{project.title}</h3>
-            <p>{project.details}</p>
-            <Button>VISIT SITE</Button>
+            <ProjectItemInner>
+              <h3>{project.title}</h3>
+              <p>{project.details}</p>
+              <Button as='a' href={project.website}>
+                VISIT SITE
+              </Button>
+            </ProjectItemInner>
           </ProjectItem>
         ))}
       </ProjectSection>
@@ -38,11 +46,14 @@ const ProjectSection = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+  align-items: flex-start;
 `;
 
 const ProjectItem = styled.div`
   width: 100%;
+  height: 100%;
   padding: 2rem;
+  flex-direction: column;
 
   @media (min-width: 768px) {
     width: 50%;
@@ -52,6 +63,10 @@ const ProjectItem = styled.div`
   }
 `;
 
+const ProjectItemInner = styled.div`
+  flex: 1; /* This makes it take up all available vertical space */
+`;
+
 const Image = styled.img`
   width: 100%;
   &:hover {
@@ -59,9 +74,13 @@ const Image = styled.img`
   }
 `;
 
-const Button = styled.button`
+const Button = styled.a`
+  display: flex;
+  justify-content: center;
   width: 100%;
   background: none;
+  text-decoration: none;
+  color: black;
   border: black 1px solid;
   border-radius: 2rem;
   padding-top: 0.75rem;
