201c2bf1670cae177cec92dffd499ca8cce1f3b9
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-09-06 08:39:51 +0200

    Feat: School address visible on map marker click.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,20 @@
-import { Wrapper } from '@googlemaps/react-wrapper';
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 export default function Map() {
-  return (
-    <Wrapper apiKey={import.meta.env.VITE_API_KEY}>
-      <MapContainer />
-    </Wrapper>
-  );
+  const render = (status: Status) => {
+    switch (status) {
+      case Status.LOADING:
+        return <MapLoading />;
+      case Status.FAILURE:
+        return <MapErrorComponent />;
+      case Status.SUCCESS:
+        return <MapContainer />;
+    }
+  };
+
+  return <Wrapper apiKey={import.meta.env.VITE_API_KEY} libraries={['places']} render={render} />;
 }
 
 function MapContainer() {
@@ -19,10 +26,48 @@ function MapContainer() {
       zoom: 15,
     });
 
-    new google.maps.Marker({
-      map: map,
-      position: { lat: 57.710467953735744, lng: 11.994911438107 },
-      title: 'Medieinstitutet, Göteborg',
+    const request = {
+      placeId: 'ChIJPe7L6Q3zT0YR9m2RYyuQP7s',
+      fields: ['name', 'formatted_address', 'geometry'],
+    };
+
+    const infowindow = new google.maps.InfoWindow();
+    const service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, (place, status) => {
+      if (
+        status === google.maps.places.PlacesServiceStatus.OK &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        const marker = new google.maps.Marker({
+          map,
+          position: place.geometry.location,
+        });
+
+        google.maps.event.addListener(marker, 'click', () => {
+          const content = document.createElement('div');
+
+          const nameElement = document.createElement('h2');
+
+          nameElement.textContent = place.name!;
+          content.appendChild(nameElement);
+
+          const placeIdElement = document.createElement('p');
+
+          placeIdElement.textContent = place.place_id!;
+          content.appendChild(placeIdElement);
+
+          const placeAddressElement = document.createElement('p');
+
+          placeAddressElement.textContent = place.formatted_address!;
+          content.appendChild(placeAddressElement);
+
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        });
+      }
     });
   }, []);
 
@@ -32,4 +77,16 @@ function MapContainer() {
 const MapInner = styled.div`
   height: 100%;
   width: 100%;
+`;
+
+function MapLoading() {
+  return <MapCaption>Loading...</MapCaption>;
+}
+
+function MapErrorComponent() {
+  return <MapCaption>Error loading map.</MapCaption>;
+}
+
+const MapCaption = styled.h2`
+  color: #f0eee8;
 `;
